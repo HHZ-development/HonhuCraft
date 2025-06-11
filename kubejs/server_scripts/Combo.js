472cf60d09e281d4666ce5a3bb5bfcf60eb78943
Yuzcen <2405161906@qq.com>
2025-06-11 19:00:56 +0800

    合并

--- kubejs/server_scripts/Combo.js
+++ kubejs/server_scripts/Combo.js
@@ -47,3 +47,16 @@ EntityEvents.hurt(event => {
         player.runCommandSilent(`title @s actionbar {"text":"${message}","color":"green"}`);
     } catch (e) {}
 });
+
+EntityEvents.death(event => {
+    let entity = event.entity;
+    let source = event.source;
+    let player = source.getPlayer();
+
+    if (player) {
+        try {
+            let command = 'playsound minecraft:entity.experience_orb.pickup voice @s[distance=..16] ~ ~ ~ 1 1';
+            player.runCommandSilent(command);
+        } catch (e) {}
+    }
+});
